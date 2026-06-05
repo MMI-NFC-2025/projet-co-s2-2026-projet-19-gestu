b7819e04dde21e8eca9aa2b23e0e00b467d6ff3b
Clement047 <clement.bertrand02@edu.univ-fcomte.fr>
2026-06-05 05:34:07 +0200

    chatdeteam

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import alpinejs from '@astrojs/alpinejs';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [alpinejs()],
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   vite: {
     plugins: [tailwindcss()],
   },
